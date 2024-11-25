43f8e82b3c5405d2aef4012482d6426d8c114639
Ibrahim Sikder <ibrahimsikder5033@gmail.com>
2024-11-25 15:30:46 +0600

    committe

--- src/app/(mainLayout)/videos/page.tsx
+++ src/app/(mainLayout)/videos/page.tsx
@@ -28,7 +28,7 @@ const VideoGallery = () => {
 
   return (
     <div> 
-      <CommonBanner title={language === 'ENG' ? 'Videos Gallery' : 'ভিডিও গ্যালারি'}/>
+      <CommonBanner title={language === 'ENG' ? 'Video Gallery' : 'ভিডিও গ্যালারি'}/>
      
       <Container>
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5 my-16">
